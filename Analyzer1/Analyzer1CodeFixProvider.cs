7d8c4a6fb967fdec7a0076ac82e37d3f211b2d57
Austin Wise <AustinWise@gmail.com>
2019-10-06 09:51:00 -0700

    Add missing using statements.

--- Analyzer1/Analyzer1CodeFixProvider.cs
+++ Analyzer1/Analyzer1CodeFixProvider.cs
@@ -14,6 +14,8 @@ using Microsoft.CodeAnalysis.Rename;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.CodeAnalysis.Operations;
 using Microsoft.CodeAnalysis.Editing;
+using System.Reflection;
+using Microsoft.CodeAnalysis.Host;
 
 namespace Analyzer1
 {
@@ -21,6 +23,45 @@ namespace Analyzer1
     public class Analyzer1CodeFixProvider : CodeFixProvider
     {
         private const string title = "Call ToString(CultureInfo.InvarientCulture)";
+
+        static readonly Func<Document, CancellationToken, Task<Document>> sAddImportsAsync;
+
+        static Analyzer1CodeFixProvider()
+        {
+            var workspaceModule = typeof(ExtensionOrderAttribute).Module;
+
+            var codeGenType = workspaceModule.GetType("Microsoft.CodeAnalysis.CodeGeneration.ICodeGenerationService", throwOnError: false, ignoreCase: false);
+            if (codeGenType == null)
+                return;
+
+            var codeGenerationOptionsType = workspaceModule.GetType("Microsoft.CodeAnalysis.CodeGeneration.CodeGenerationOptions", throwOnError: false, ignoreCase: false);
+            if (codeGenerationOptionsType == null)
+                return;
+
+            var addImportsMethod = codeGenType.GetMethod("AddImportsAsync",
+                new Type[] { typeof(Document), codeGenerationOptionsType, typeof(CancellationToken) });
+            if (addImportsMethod == null)
+                return;
+
+            var defaultProp = codeGenerationOptionsType.GetField("Default", BindingFlags.Static | BindingFlags.Public);
+            if (defaultProp == null)
+                return;
+            object defaultValue = defaultProp.GetValue(null);
+
+            var getServiceFunction = typeof(HostLanguageServices).GetMethod(nameof(HostLanguageServices.GetService));
+            if (getServiceFunction == null)
+                return;
+
+            var getGenerationService = getServiceFunction.MakeGenericMethod(codeGenType);
+
+            sAddImportsAsync = (doc, ct) =>
+            {
+                var codeGenService = getGenerationService.Invoke(doc.Project.LanguageServices, null);
+                if (codeGenService == null)
+                    return Task.FromResult(doc);
+                return (Task<Document>)addImportsMethod.Invoke(codeGenService, new object[] { doc, defaultValue, ct });
+            };
+        }
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
@@ -61,19 +102,24 @@ namespace Analyzer1
                 return document;
             }
 
-            DocumentEditor editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
-            SyntaxGenerator generator = editor.Generator;
+            var semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+            var cultureInfoType = semanticModel.Compilation.GetTypeByMetadataName("System.Globalization.CultureInfo");
 
-            var cultureInfoType = editor.SemanticModel.Compilation.GetTypeByMetadataName("System.Globalization.CultureInfo");
+            SyntaxGenerator generator = SyntaxGenerator.GetGenerator(document);
 
+            //add the ToString(CultureInfo.InvarientCulture)
             var toStringAccess = generator.MemberAccessExpression(conversionNode, "ToString");
             var cultureInfoTypeExpr = generator.TypeExpression(cultureInfoType, addImport: true);
             var invarientCultureExpr = generator.MemberAccessExpression(cultureInfoTypeExpr, "InvariantCulture");
             var toStringInvoke = generator.InvocationExpression(toStringAccess, invarientCultureExpr);
+            root = root.ReplaceNode(conversionNode, toStringInvoke);
+            document = document.WithSyntaxRoot(root);
 
-            editor.ReplaceNode(conversionNode, toStringInvoke);
+            //add using to the top of the file if it is not there already
+            if (sAddImportsAsync != null)
+                document = await sAddImportsAsync(document, cancellationToken).ConfigureAwait(false);
 
-            return editor.GetChangedDocument();
+            return document;
         }
     }
 }
